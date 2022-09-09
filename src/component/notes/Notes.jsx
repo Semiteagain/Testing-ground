f3cd03587e4a9bfa952d1821fc86eff2859dcaf8
Semiu Mukaila <semiteagain@gmail.com>
2022-09-09 18:37:34 +0100

    delete object with filter function

--- src/component/notes/Notes.jsx
+++ src/component/notes/Notes.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./notes.css"
 
 const Notes = (props) => {
+
+  const handleClick = () => {
+    props.onDelete(props.id);
+  }
   return (
     <div className="container__note">
 <div className='note'>
         <h1>{props.title}</h1>
         <p>{props.content} </p>
-        <button >Delete</button>
+        <button onClick={handleClick}>Delete</button>
         </div>
     </div>
     
